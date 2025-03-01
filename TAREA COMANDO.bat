2b0ac8223838d96472f4d8c46880d69f60ed6990
ALONSO93MARTINEZ <mtz.93.alo@gmail.com>
2025-03-01 01:25:22 -0600

    Add files via upload

---
+++ TAREA COMANDO.bat
@@ -0,0 +1,15 @@
+cd TAREA COMANDO
+mkdir Tarea_01
+
+Python -m venv mi_entorno
+call mi_entorno\Scripts\actívate
+
+pip install pandas 
+pip install numpy
+pip install matplotlib
+
+pip freeze > requirements.txt
+
+desactivate
+
+pause nul
